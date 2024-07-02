8189af8c99e148fa81b82e27f47f8d781976b277
Anukriti <97687618+anuukriti@users.noreply.github.com>
2024-07-03 00:57:45 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ function About() {
                 <div className="sm:px-10 h-auto w-full flex py-8 lg:py-0 flex-col items-start ">
                     <p className="font-Font1 tracking-widest text-md  leading-8 text-justify">
                         <span className="text-4xl ">Hi,</span> <br />  I am final
-                        year undergraduate student at IIT (BHU) Varanasi. I am currently working as a <span className="text-fuchsia-400">UI/UX desiginer </span> and as a <span className="text-fuchsia-400">frontened engineer </span>at charan enterprises and mentorify respectively. I have deep interest in website development,
+                        year undergraduate student at IIT (BHU) Varanasi. I am currently working as a <span className="text-fuchsia-400">UI/UX desiginer </span> and as a <span className="text-fuchsia-400">frontened engineer </span>at charan enterprises. I have deep interest in website development,
                         UI/UX desigining, graphic design, and software development. I
                         also work as a design head in the Society of Chemical
                         Engineering (SoChem) in IIT BHU. <br /><br />I enjoy taking on
@@ -30,6 +30,49 @@ function About() {
                             I am open to new opportunities. Please review my profile and feel free to contact me.
                         
                     </p>
+
+                    <div className="flex flex-col justify-center sm:justify-start w-full text-center sm:flex-row gap-5 mt-5">
+                        <p className="text-fuchsia-400 font-semibold font-Font1 tracking-widest text-md  leading-8 text-justify py-5">
+                        
+                            My Top Skills: 
+                    </p>
+                        <br/>
+                        <li
+                            className="py-2 border-2 border-transparent hover:border-white bg-fuchsia-400 hover:bg-fuchsia-300 transition-all ease-linear rounded-2xl px-5 text-fuchsia-950 font-bold p-2"
+                        >
+                            React JS
+                        </li>
+
+                        <li
+                            className="py-2 border-2 border-transparent hover:border-white bg-fuchsia-400 hover:bg-fuchsia-300 transition-all ease-linear rounded-2xl px-5 text-fuchsia-950 font-bold p-2"
+                        >
+                            Tailwind CSS
+                        </li>
+
+                        <li
+                            className="py-2 border-2 border-transparent hover:border-white bg-fuchsia-400 hover:bg-fuchsia-300 transition-all ease-linear rounded-2xl px-5 text-fuchsia-950 font-bold p-2"
+                        >
+                            JavaScript
+                        </li>
+
+                        <li
+                            className="py-2 border-2 border-transparent hover:border-white bg-fuchsia-400 hover:bg-fuchsia-300 transition-all ease-linear rounded-2xl px-5 text-fuchsia-950 font-bold p-2"
+                        >
+                            Appwrite
+                        </li>
+
+                        <li
+                            className="py-2 border-2 border-transparent hover:border-white bg-fuchsia-400 hover:bg-fuchsia-300 transition-all ease-linear rounded-2xl px-5 text-fuchsia-950 font-bold p-2"
+                        >
+                            Figma
+                        </li>
+                        <li
+                            className="py-2 border-2 border-transparent hover:border-white bg-fuchsia-400 hover:bg-fuchsia-300 transition-all ease-linear rounded-2xl px-5 text-fuchsia-950 font-bold p-2"
+                        >
+                            Adobe Illustrator
+                        </li>
+                       
+                    </div>
 
                     <div className="flex flex-col justify-center sm:justify-start w-full text-center sm:flex-row gap-5 mt-5">
                         <Link
